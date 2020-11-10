e459c9ecbdc540d6b7534d9ab944b1031705a046
zaifrun <martinknud@gmail.com>
2020-11-10 16:29:13 +0100

    Add Kmeans example

--- lektion6/DBScan_example.py
+++ lektion6/DBScan_example.py
@@ -9,25 +9,33 @@ from sklearn.preprocessing import StandardScaler
 
 # #############################################################################
 # Generate sample data
-centers = [[1, 1], [-1, -1], [1, -1]]
+centers = [[1, 1], [-1, -1], [1, -1]]  # we want 3 centers
+#make some random data, 750 data points around the centers, with a standard deviation size of 0.4
 X, labels_true = make_blobs(n_samples=750, centers=centers, cluster_std=0.4,
                             random_state=0)
 
-X = StandardScaler().fit_transform(X)
+X = StandardScaler().fit_transform(X) #scaling the values
 
 # #############################################################################
 # Compute DBSCAN
 db = DBSCAN(eps=0.3, min_samples=10).fit(X)
+#initialize an array of zeros (i.e. false) - the array has an index for every point
 core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+#assign true to the all the samles indicated as core_points by the algorithm
 core_samples_mask[db.core_sample_indices_] = True
-labels = db.labels_
+labels = db.labels_  # the array of labels assigned to the points - i.e. cluster numbers
 
-# Number of clusters in labels, ignoring noise if present.
+# Number of clusters in labels, ignoring noise if present
+# Noise is also assigned a label (-1), all the real clusters are assigned
+# values from 0,1,2...and so on. So if there is noise, then subtract 1 from the size of
+# the labels to get the number of clusters.
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-n_noise_ = list(labels).count(-1)
+n_noise_ = list(labels).count(-1) # count the number of points that is labelled noise
 
 print('Estimated number of clusters: %d' % n_clusters_)
 print('Estimated number of noise points: %d' % n_noise_)
+#various performance scores - you can look them up the in the documentation for
+#DB scan
 print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
 print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
 print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
@@ -39,7 +47,7 @@ print("Silhouette Coefficient: %0.3f"
       % metrics.silhouette_score(X, labels))
 
 # #############################################################################
-# Plot result
+# Plotting the  result
 import matplotlib.pyplot as plt
 
 # Black removed and is used for noise instead.
@@ -57,19 +65,26 @@ ax.legend()
 ax.grid(True)
 
 plt.figure()
+#make some colors along a gradient from 0 to 1 in the colorspace and we need the number of labels = number
+#of colors
 colors = [plt.cm.Spectral(each)
           for each in np.linspace(0, 1, len(unique_labels))]
+#loop over all the labels (1 for every point) and also map a label to a color at the same time.
 for k, col in zip(unique_labels, colors):
+    # is it a noise label? (-1)
     if k == -1:
         # Black used for noise.
         col = [0, 0, 0, 1]
 
+   
     class_member_mask = (labels == k)
 
+    # the core points have a different size - the are bigger
     xy = X[class_member_mask & core_samples_mask]
     plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
              markeredgecolor='k', markersize=14)
 
+    # the non-core points will be smaller in size - but they still belong to the cluster of course
     xy = X[class_member_mask & ~core_samples_mask]
     plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
              markeredgecolor='k', markersize=6)
